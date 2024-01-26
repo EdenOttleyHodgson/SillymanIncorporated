1dce042de1a9a8e62a1601bb6cdc2df7bcdb0ce0
coelacanth940 <jd7g22@soton.ac.uk>
2024-01-26 21:12:23 +0000

    Hit detection added

--- Scenes/Track/track.gd
+++ Scenes/Track/track.gd
@@ -1,21 +1,25 @@
 extends Area2D
 signal hit
-
+var notesInside
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	notesInside = []
+	pass
 
-
+func _input(event):
+	if event.is_action_pressed("hit_note1"):
+		if len(notesInside) > 0:
+			notesInside.pop_front().free()
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
-
-	
-
-
-
-
-
 func _on_area_entered(area):
-	print("fire in the hole")
+	notesInside.append(area)
+	print(notesInside)
+
+func _on_area_exited(area):
+	notesInside.erase(area)
+	print(notesInside)
+
