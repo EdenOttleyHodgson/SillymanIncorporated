9417a6b174c9deeae713985c6c530690b2b9b9bb
coelacanth940 <jd7g22@soton.ac.uk>
2024-01-27 14:36:43 +0000

    Scoring and HUD

--- Scenes/Track/track.gd
+++ Scenes/Track/track.gd
@@ -1,28 +1,39 @@
 extends Area2D
 signal hit
 var notesInside
+var hitWidth
+var score = 0
+var combo = 0
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	notesInside = []
-	pass
+	hitWidth = $hitbox.shape.extents.x # would also need to add radius of note
 
 func _input(event):
 	if event.is_action_pressed("hit_note1"):
-		print("fire in the hole")
 		if len(notesInside) > 0:
-			var distance = notesInside[0].position.x-position.x
-			print(distance)
-			#Define distance ranges for points
-			if distance > -25 && distance < 25:
+			var currentNote = notesInside[0]
+			var distance = currentNote.position.x-position.x
+			var totalDistance = hitWidth + (currentNote.get_node("CollisionShape2D").shape.radius)/2
+			# Define distance ranges for points
+			# TODO maybe change range based on speed? - probably convert to time using t=d/s
+			var perfect = totalDistance/5
+			var okay = totalDistance/3
+			
+			if distance > -(perfect) && distance < (perfect): # These numbers will need tweaking
 				print("perfect")
-			elif distance > -50 && distance < 50:
+				score += 300
+			elif distance > -(okay) && distance < okay:
 				print("okay")
+				score += 100
 			else:
 				print("bad")
-			
+				score += 50
+			combo += 1
 			# Pop note from list and delete it
 			notesInside.pop_front().free()
-			
+			hit.emit()
 			
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -30,9 +41,12 @@ func _process(delta):
 
 func _on_area_entered(area):
 	notesInside.append(area)
-	print(notesInside)
 
 func _on_area_exited(area):
 	notesInside.erase(area)
-	print(notesInside)
 
+func get_score():
+	return score
+
+func get_combo():
+	return combo
--- Scenes/node_2d.gd
+++ Scenes/node_2d.gd
@@ -1,17 +1,17 @@
-extends Area2D
-var speed = 500
-
+extends Node2D
+var health = 100
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	position.x = position.x - speed * delta
+	health -= 1
+	$health.size.x = health
 
-func get_speed():
-	return speed
 
-func set_speed(newSpeed):
-	speed = newSpeed
+func _on_track_hit():
+	$score.text = "Score: " + str($track.get_score())
+	$combo.text = str($track.get_combo()) + "x"
